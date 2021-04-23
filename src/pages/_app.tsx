f7912ab1a27cd71da090b84c485efea77538207f
luizmn <luizmn@outlook.com>
2021-04-23 14:22:26 +0100

    separated player context + prepare for webpack 5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,39 +2,11 @@ import Header from "../components/Header";
 import Player from "../components/Player";
 import "../styles/global.scss";
 import styles from "../styles/app.module.scss";
-import { PlayerContext } from "../contexts/PlayerContext";
-import { useState } from "react";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 
 function App({ Component, pageProps }) {
-
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay(episode) {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
-  return (
-    <PlayerContext.Provider 
-    value={{ 
-      episodeList, 
-      currentEpisodeIndex, 
-      play, 
-      isPlaying, 
-      togglePlay,
-      setPlayingState
-    }}>
+    return (
+      <PlayerContextProvider>
       <div className={styles.wrapper}>
             <main>
               <Header />
@@ -42,7 +14,7 @@ function App({ Component, pageProps }) {
             </main>
             <Player />
       </div>
-    </PlayerContext.Provider>
+      </PlayerContextProvider>
     )
 }
 
