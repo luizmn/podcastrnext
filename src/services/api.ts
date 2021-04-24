f6fefee01d6c24509a1651826ad3a11b808fcbbe
luizmn <luizmn@outlook.com>
2021-04-24 21:52:27 +0100

    OK

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+    // baseURL: 'http://localhost:3333/'
+    baseURL: 'https://jsonstorage.net/api/items/9bf947fd-fa86-4aac-b274-e06e763fe721'
 })
